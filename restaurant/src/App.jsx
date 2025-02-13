c86e5afa97ad69ff5cbbee4ee3a478e0edb51dcb
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-02-13 12:31:14 +0100

    Node

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -1,9 +1,34 @@
 const App = ()=>{
     return (
         <div>
-            <div className="navbar bg-red">
-
+            <div className="navbar">
+                <nav>
+                    <div>
+                        <h1>
+                            <a href="#">Food Ninja</a>
+                        </h1>
+                    </div>
+                    <ul>
+                        <li><a href="#"><span>Home</span></a></li>
+                        <li><a href="#"><span>About</span></a></li>
+                        <li><a href="#"><span>Contact</span></a></li>
+                    </ul>
+                </nav>
             </div>
+
+            <main>
+                <div>
+                    <a href="#">Log in</a>
+                    <a href="#">Sign up</a>
+                </div>
+
+                <header>
+                    <h2>Receipes</h2>
+                    <h3>For Customers</h3>
+                </header>
+            </main>
         </div>
     )
 }
+
+export default App
